b09fec190a257e47ea9c263292874706a5f3b5ee
gaaldvd <gaaldavid@tuta.io>
2025-04-27 18:59:16 +0200

    minor update

--- useful/scripts/start.sh
+++ useful/scripts/start.sh
@@ -1,20 +1,42 @@
 # validate options
-if [ "$#" -eq 0 ]; then
-    echo "> Usage: $0 [-c/-g]"
+if [ "$#" -eq 0 ] || [[ "$1" != "-u" && "$1" != "-c" && "$1" != "-g" ]]; then
+    echo "> Usage: $0 [-u] [-c/-g] [params]"
     exit 1
 fi
 
 # interpret options
-while getopts "cg" flag; do
+while getopts "ucg" flag; do
   case $flag in
-    c) mode="cli";;
-    g) mode="gui";;
+    u)
+      echo "> Updating..."
+      ./update.sh
+      exit 0;;
+    c)
+      mode="cli"
+      break;;
+    g)
+      mode="gui"
+      break;;
     *)
-      echo "> Usage: $0 [-c/-g]"
+      echo "> Usage: $0 [-u] [-c/-g] [params]"
       exit 1;;
   esac
 done
 
 # start application
 clear
-pipenv run python script_$mode.py
+if [ -n "$2" ]; then
+    params="$2"
+    echo "> Parameters: $params"
+    shift 2
+    if [ "$#" -eq 0 ]; then
+        pipenv run python src/script_"$mode".py "$params"
+    else
+        args="$@"
+        echo "> Arguments: $args"
+        pipenv run python src/script_"$mode".py "$params" "$args"
+    fi
+else
+    echo "> No parameters/arguments specified."
+    pipenv run python src/script_"$mode".py
+fi
